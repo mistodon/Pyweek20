765c1187476e7b8b98dae494fbbfc524f61fc583
Vi <1149443+mistodon@users.noreply.github.com>
2015-08-11 21:50:43 +0100

    Robot can be moved by drum commands. However, timing is not taken account of.

--- datapawn/datapawn/entity.py
+++ datapawn/datapawn/entity.py
@@ -2,15 +2,21 @@ import pyglet
 
 
 class Entity:
-    def __init__(self, pos, *components):
+    def __init__(self, window, pos, name=None, components=[]):
+        self.window = window
         self.pos = pos
+        self.name = name
         self.components = components
         for c in components:
-            c.entity = self
+            c.attach(self)
 
-class Component:
+class Component():
     def __init__(self):
         self.entity = None
+
+    def attach(self, entity):
+        self.entity = entity
+        entity.window.push_handlers(self)
 
 class Drawable(Component):
     def __init__(self, image, batch=None):
@@ -18,5 +24,23 @@ class Drawable(Component):
         img = pyglet.resource.image(image)
         self.sprite = pyglet.sprite.Sprite(img, batch=batch)
 
-    def tick(self, dt):
+    def on_tick(self, dt):
         self.sprite.set_position(*self.entity.pos)
+
+class Datapawn(Component):
+    def __init__(self):
+        super().__init__()
+        self.dest = None
+
+    def on_drum_command(self, command):
+        if command == ["D", "D", "D", "1"]:
+            self.dest = self.entity.pos[0] + 100
+
+    def on_tick(self, dt):
+        if self.dest is not None:
+            x,y = self.entity.pos
+            dx = -50*dt if self.dest < x else 50*dt
+            newx = x + dx
+            if (x < self.dest < newx) or (newx < self.dest < x):
+                self.dest = None
+            self.entity.pos = (newx, y)
--- datapawn/datapawn/gamescreen.py
+++ datapawn/datapawn/gamescreen.py
@@ -1,8 +1,15 @@
 import pyglet
 import pyglet.gl as gl
+from pyglet.window import key
 
-from datapawn.entity import Entity, Drawable
+from datapawn.entity import Entity, Drawable, Datapawn
 
+CONTROLS = {
+    key.UP: "D",
+    key.DOWN: "0",
+    key.LEFT: "-",
+    key.RIGHT: "1",
+}
 
 class GameScreen(pyglet.window.Window):
     GROUND_Y = 50
@@ -11,18 +18,36 @@ class GameScreen(pyglet.window.Window):
         super().__init__(800, 450)
         gl.glClearColor(0.5,0.85,1.0,1.0)
         self.batch = pyglet.graphics.Batch()
+        self.named_entities = {}
 
         self.entities = [
-            Entity((10, self.GROUND_Y),
-                Drawable(image="datapawn.png", batch=self.batch))
+            Entity(self, (10, self.GROUND_Y), name="Robot0", components=[
+                Drawable(image="datapawn.png", batch=self.batch),
+                Datapawn()
+                ])
             ]
-
-        pyglet.clock.schedule_interval(self.on_tick, 1.0/60.0)
-
-    def on_tick(self, dt):
         for e in self.entities:
-            for c in e.components:
-                c.tick(dt)
+            if e.name:
+                self.named_entities[e.name] = e
+
+        pyglet.clock.schedule_interval(self.tick, 1.0/60.0)
+        self.command = []
+
+    def on_key_press(self, symbol, modifiers):
+        super().on_key_press(symbol, modifiers)
+        sym = CONTROLS.get(symbol)
+        if sym:
+            self.command.append(sym)
+            if len(self.command) == 4:
+                self.dispatch_event("on_drum_command", self.command)
+                self.command = []
+
+    def tick(self, dt):
+        self.dispatch_event("on_tick", dt)
+        robot0 = self.named_entities["Robot0"]
+        if robot0.pos[0] > 750:
+            print("Victory!")
+            pyglet.app.exit()
 
     def on_draw(self):
         self.clear()
@@ -38,3 +63,6 @@ class GameScreen(pyglet.window.Window):
         colors = (1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0)
         pyglet.graphics.draw(6, pyglet.gl.GL_TRIANGLE_STRIP,
             ("v2f", groundstrip), ("c3f", colors))
+
+GameScreen.register_event_type("on_tick")
+GameScreen.register_event_type("on_drum_command")
